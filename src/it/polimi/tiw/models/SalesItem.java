f48e1905e3c05694edf81f986b076d846e6ac2b1
NicolaDean <nicola.dean@mail.polimi.it>
2021-04-07 17:39:52 +0200

    Added SalesItem model

--- src/it/polimi/tiw/models/SalesItem.java
+++ src/it/polimi/tiw/models/SalesItem.java
@@ -1,4 +1,40 @@
 package it.polimi.tiw.models;
 
 public class SalesItem {
+
+    private int code;
+    private String name;
+    private String description;
+    private String pathImage;
+
+    public SalesItem(int code,String name,String description,String pathImage)
+    {
+        this.code           = code;
+        this.name           = name;
+        this.description    =description;
+        this.pathImage      = pathImage;
+    }
+
+    public int getCode()
+    {
+        return this.code;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public String getDescription()
+    {
+        return this.description;
+    }
+
+    public String getPathImage()
+    {
+        return this.pathImage;
+    }
+
+
+
 }
