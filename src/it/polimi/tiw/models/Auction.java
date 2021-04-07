aa2cbf1e85533d2cccd7254e3df2f64ba3ca4053
NicolaDean <nicola.dean@mail.polimi.it>
2021-04-07 17:31:47 +0200

    Added User model

---
+++ src/it/polimi/tiw/models/Auction.java
@@ -0,0 +1,4 @@
+package it.polimi.tiw.models;
+
+public class Auction {
+}
---
+++ src/it/polimi/tiw/models/User.java
@@ -0,0 +1,30 @@
+package it.polimi.tiw.models;
+
+public class User {
+    private int id;
+    private String email;
+    private String name;
+
+
+    public User(int id,String email,String name)
+    {
+        this.id     = id;
+        this.email  = email;
+        this.name   = name;
+    }
+    public String getEmail()
+    {
+        return this.email;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public int getId()
+    {
+        return this.id;
+    }
+
+}
